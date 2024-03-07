8fd59eeb858fafdaec2940ca58f5541ca2e0d942
matthewblu421 <44147556+matthewblu421@users.noreply.github.com>
2024-03-06 21:30:48 -0500

    FINALLY got write capablities to firebase WOO

--- src/App.js
+++ src/App.js
@@ -1,41 +1,90 @@
-import "./App.css"
-import Navbar from "./components/Navbar"
-import MediaList from "./components/MediaList"
-import React from "react"
-import SearchBar from "./components/SearchBar"
-import { useEffect, useState } from "react"
-import SearchResultsList from "./components/SearchResultsList"
+import "./App.css";
+import Navbar from "./components/Navbar";
+import MediaList from "./components/MediaList";
+import React from "react";
+import SearchBar from "./components/SearchBar";
+import { useEffect, useState } from "react";
+import SearchResultsList from "./components/SearchResultsList";
+import { onSnapshot, addDoc } from "firebase/firestore";
+import { moviesCollection } from "./firebase.js";
 
-console.log(process.env.REACT_APP_OMDB_API_KEY)
+//#TODO: Add a button to add a movie FROM THE SEARCH BAR to the watched list!!!!
+
+//#TODO: Add a button to add a movie FROM THE SEARCH BAR to the watched list!!!!
+
+//#TODO: Add a button to add a movie FROM THE SEARCH BAR to the watched list!!!!
+
+//#TODO: Add a button to add a movie FROM THE SEARCH BAR to the watched list!!!!
+
+//#TODO: Add a button to add a movie FROM THE SEARCH BAR to the watched list!!!!
+
+//#TODO: Add a button to add a movie FROM THE SEARCH BAR to the watched list!!!!
+
+//#TODO: Add a button to add a movie FROM THE SEARCH BAR to the watched list!!!!
 
 function App() {
-  const [mediaName, setMediaName] = useState("")
+  const [movie, setMovie] = useState("");
+  const [watchedList, setWatchedList] = useState([]);
+  const [movieId, setMovieId] = useState(watchedList[0]?.id | "");
+  const [searchResults, setSearchResults] = useState([]);
 
-  const [watchedList, setWatchedList] = useState([])
+  //#TODO: Add a button to add a movie FROM THE SEARCH BAR to the watched list!!!!
+  //#TODO: Add a button to add a movie FROM THE SEARCH BAR to the watched list!!!!
+  //#TODO: Add a button to add a movie FROM THE SEARCH BAR to the watched list!!!!
 
-  const [searchResults, setSearchResults] = useState([])
+  useEffect(() => {
+    const unsubscribe = onSnapshot(moviesCollection, function (snapshot) {
+      //sync local movie list with snapshot data
+      const moviesArray = snapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setWatchedList(moviesArray);
+    });
+    return unsubscribe;
+  }, []);
 
-  useEffect(() => console.log(watchedList), [watchedList])
+  // async function addToWatched() {
+  //   const newMovie = {
+  //     title: "",
+  //     body: "#Type your movie review here!",
+  //   };
+  //   const addMovieRef = await addDoc(moviesCollection, newMovie);
+  //   setMovieId(addMovieRef.id);
+  //   // setWatchedList((oldList) => [...oldList, movie])
+  //   // setMovie("")
+  // }
 
   function handleChange(event) {
-    const { value, name } = event.target
-    setMediaName(value)
-    console.log(watchedList)
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault()
-    setWatchedList((oldList) => [...oldList, mediaName])
-    setMediaName("")
+    const { value } = event.target;
+    setMovie(value);
+    console.log(watchedList);
   }
 
   function resetWatchedList() {
-    setWatchedList([])
+    setWatchedList([]);
   }
 
-  const listElements =
-    watchedList.length > 0 &&
-    watchedList.map((watchedItem) => <li>{watchedItem}</li>)
+  // const listElements =
+  //   watchedList.length > 0 &&
+  //   watchedList.map((watchedItem) => <li>{watchedItem}</li>);
+
+  async function addNewMovie(event) {
+    event.preventDefault()
+  const newMovie = {
+      title: movie,
+      body: "#Type your review here!"
+  }
+  const newMovieRef = await addDoc(moviesCollection, newMovie);
+  setMovieId(newMovieRef.id);
+}
+
+
+  // function handleSubmit(event) {
+  //   event.preventDefault()
+  //   setWatchedList((oldList) => [...oldList, movie])
+  //   setMovie("")
+  // }
 
   return (
     <main className="main--wrapper">
@@ -46,23 +95,28 @@ function App() {
       <Navbar />
       <MediaList
         resetWatchedList={resetWatchedList}
-        listElements={listElements}
+        // listElements={listElements}
       />
       <div className="input--form--container">
-        <form className="title-form" onSubmit={handleSubmit}>
+        <form onSubmit={addNewMovie} className="title-form">
           <input
             type="text"
             placeholder="Enter Title"
             className="title--form--input"
             name="title"
+            id={movie}
+            // onSubmit={handleSubmit}
+            
             onChange={handleChange}
-            value={mediaName}
+            value={movie}
           />
-          <button className="submit--btn">Add Movie</button>
+          <button className="submit--btn">
+            Add Movie
+          </button>
         </form>
       </div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,42 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore, collection} from "firebase/firestore";
+
+
+// # Don't need analytics right now
+//import { getAnalytics } from "firebase/analytics";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDTrW-aL0UnC1Vru2zfux9-RiOwdAYOG18",
+
+  authDomain: "seenapp-8cd15.firebaseapp.com",
+
+  databaseURL: "https://seenapp-8cd15-default-rtdb.firebaseio.com",
+
+  projectId: "seenapp-8cd15",
+
+  storageBucket: "seenapp-8cd15.appspot.com",
+
+  messagingSenderId: "364925691012",
+
+  appId: "1:364925691012:web:73f484d1f1cb6eb4154ed1",
+
+  measurementId: "G-9NRP14WJX8"
+
+};
+
+
+// Initialize Firebase
+// const analytics = getAnalytics(app);
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export const moviesCollection = collection(db, "SeenApp");
